4529bd76db41a531eaf270608c97b75716abc4f6
artifishional <artifishional@mail.ru>
2018-02-01 10:55:36 +0300

    -init

---
+++ src/container.js
@@ -0,0 +1,63 @@
+import {forEachFromData} from "./utils"
+import {Loader, Error} from "m2"
+
+/**
+ * Органайзер списка
+ * @param scheme
+ * @param stream
+ * @param {Array<String>} signature
+ */
+export default function container({scheme, stream, signature}) {
+
+    //коллекция компонентов
+    const plugs = scheme.constructor = [
+
+        /**
+         * Плагин, который определяет сигнатуру для поиска
+         */
+        function ({scheme, ...data}) {
+            signature.map( prop => scheme[prop] = data[prop] )
+        }
+        
+    ];
+
+    //управление элементами
+    scheme.item = stream.map( forEachFromData( ({data, type}) => {
+
+        if(type === "reinit") {
+
+            const loader = new Loader(data);
+
+            const scheme = {};
+
+            function onload() {
+                plugs.every( plug => plug({ scheme, stream, ...data} ) );
+            }
+
+            stream = stream.withHandler(function (emt, data) {
+                if(!loader.isLoad) {
+                    loader.onload.off(onload);
+                    loader.onload.on(onload);
+                    return new Error({ type: "MODULE_NOT_READY", event: loader.onload });
+                }
+                emt.emit(data);
+            });
+
+            return {scheme, stream};
+
+        }
+        else if(type === "add") {
+
+
+        }
+
+
+    } ) );
+
+}
+
+
+/**
+ * stream
+ * {type: "reinit", data: [ signature ]}
+ */
---
+++ src/st_child.js
@@ -0,0 +1,18 @@
+import list from "./container";
+
+export default function stChild({scheme, stream}) {
+
+    scheme.
+
+}
+
+
+
+const schema =
+
+    ["name",
+        ["name", "string", { len: 5, def: "1.0.0" }],
+        ["name", "string", { len: 5, def: "1.0.0" }]
+    ]
+
+;
