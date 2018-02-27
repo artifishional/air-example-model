3db427e4385465ea31f50db48c6e137791dc614d
artifishional <artifishional@mail.ru>
2018-02-28 02:28:21 +0300

    - lazy list exmp - added

--- src/list.js
+++ src/list.js
@@ -6,9 +6,31 @@ export default class List extends Observable {
         super( function (emt) {
             stream.on( ({type, list, ...args }) => {
                 if(type === "reinit") {
-                    emt.emit({ type, list: list.map( x => this.creators.find( creator => creator(x) )({data: x, stream})), ...args});
+                    emt.emit({ type, list: list.map( x => {
+
+                        //todo need cache
+                        //todo need overloop
+                        //todo need lazy init
+
+                        const res = {};
+
+                        res.main = new Observable( (emt) => {
+
+                            const res = this.creators.find( creator => creator({data: x, stream}) )({data: x, stream});
+                            const {main} = res;
+
+                            return main.on( (data) => {
+                                emt.emit(data);
+                            } );
+
+                        } );
+
+                        return res;
+
+                    }), ...args});
+
                 }
-                //todo rework
+                //todo need rework
                 else if(type === "add") {
                     emt.emit({ type, list: list.map( x => this.creators.find( creator => creator(x) )(x)), ...args});
                 }
